be8a04d50a966dbb22cb04243d353022e8a70f00
asfaq-sufian-shamolima <asfaq.sufian@shamolima.com>
2024-03-20 17:07:46 +0600

    security code

---
+++ src/main/java/com/shamolima/locationservice/LocationService/security/JWTAuthorizationFilter.java
@@ -0,0 +1,59 @@
+package com.shamolima.locationservice.LocationService.security;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.SignedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+
+@Component
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+    //@Value("${SECRET}")
+    private String JWT_SECRET = "";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(request);
+
+        try {
+            if(validateToken(JWT_SECRET.getBytes(StandardCharsets.UTF_8), token)){
+                filterChain.doFilter(request, response);
+            } else {
+                throw new RuntimeException("Invalid token");
+            }
+
+        } catch (ParseException | JOSEException e) {
+            throw new RuntimeException("Invalid token");
+        }
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // Remove "Bearer " prefix
+        } else {
+            throw new RuntimeException("Bad request");
+        }
+    }
+
+    private boolean validateToken(byte[] secret, String token) throws ParseException, JOSEException {
+        SignedJWT receivedSignedJWT = SignedJWT.parse(token);
+        JWSVerifier verifier = new MACVerifier(secret);
+        boolean isValid = receivedSignedJWT.verify(verifier);
+        return isValid;
+    }
+
+}
---
+++ src/main/java/com/shamolima/locationservice/LocationService/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.shamolima.locationservice.LocationService.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+
+    public WebSecurityConfig(JWTAuthorizationFilter jwtAuthorizationFilter) {
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
